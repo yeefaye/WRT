36dbffa5613f2f189ff04bba5dedff46a2705f29
Ming <70466109+yeefaye@users.noreply.github.com>
2023-09-12 19:45:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwallecho' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClashcho' >>feeds.conf.default
